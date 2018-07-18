5fc9dcdeaa4637ce33a3c38e0b68caf948794b10
周景 <jing.zhou05@bqjr.cn>
2018-07-18 17:47:46 +0800

    :add getters/actions/

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,6 @@
 <template>
 <div id="app">
 	<router-view></router-view>
-	<!-- <div class="">
-		this is again
-	</div> -->
 </div>
 </template>
 
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,11 +1,27 @@
 <template>
 <div class="login">
-	this is login
+	{{getName}}
+	<button type="button" name="button" @click="updateName">更新</button>
 </div>
 </template>
 
 <script>
-export default {}
+// import store from '../store'
+export default {
+	computed: {
+		getName() {
+			// alert(99999999999)
+			return this.$store.getters.getReverseName
+		}
+
+	},
+	methods: {
+		updateName() {
+			// this.$store.commit('updateName')
+			this.$store.dispatch('updateNameAsync')
+		}
+	}
+}
 </script>
 
 <style lang="less">
